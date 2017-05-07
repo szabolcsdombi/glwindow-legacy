e398fa8ea3532dc118cc4fd2fefad22bdc98f7ec
Szabolcs Dombi <cprogrammer1994@gmail.com>
2017-05-07 20:59:27 +0200

    pre release

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ GLWindow = Extension(
 
 setup(
 	name = 'GLWindow',
-	version = '1.1.4',
+	version = '2.0.1',
 	description = 'GLWindow',
 	url = 'https://github.com/cprogrammer1994/GLWindow',
 	author = 'Szabolcs Dombi',
