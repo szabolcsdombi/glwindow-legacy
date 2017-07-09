e933b94f5401ede43f413c4f8fb046b8ebc3309f
Szabolcs Dombi <cprogrammer1994@gmail.com>
2017-07-09 16:48:20 +0300

    pep8

--- setup.py
+++ setup.py
@@ -8,24 +8,24 @@ long_description = '''
 
 GLWindow = Extension(
     'GLWindow.glwnd',
-    libraries = ['User32', 'opengl32', 'gdi32'],
-    sources = [
+    libraries=['User32', 'opengl32', 'gdi32'],
+    sources=[
         'src/GLWindow.cpp',
         'src/ModernContext.cpp',
     ]
 )
 
 setup(
-    name = 'GLWindow',
-    version = '2.2.0',
-    description = 'GLWindow',
-    long_description = long_description.strip(),
-    url = 'https://github.com/cprogrammer1994/GLWindow',
-    author = 'Szabolcs Dombi',
-    author_email = 'cprogrammer1994@gmail.com',
-    license = 'MIT',
-    packages = ['GLWindow'],
-    ext_modules = [GLWindow],
-    install_requires = ['ModernGL'],
-    platforms = ['win32', 'win64']
+    name='GLWindow',
+    version='2.2.0',
+    description='GLWindow',
+    long_description=long_description.strip(),
+    url='https://github.com/cprogrammer1994/GLWindow',
+    author='Szabolcs Dombi',
+    author_email='cprogrammer1994@gmail.com',
+    license='MIT',
+    packages=['GLWindow'],
+    ext_modules=[GLWindow],
+    install_requires=['ModernGL'],
+    platforms=['win32', 'win64']
 )
