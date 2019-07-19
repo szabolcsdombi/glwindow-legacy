d7c4feaed43cda30c7104a7437b44a909b50ce16
Szabolcs Dombi <cprogrammer1994@gmail.com>
2019-07-19 11:50:28 +0300

    fastprototype new implementation

---
+++ setup.py
@@ -0,0 +1,56 @@
+import platform
+
+from setuptools import setup, Extension
+
+PLATFORMS = {'windows', 'linux'}
+
+libraries = {
+    'windows': ['gdi32', 'opengl32', 'user32'],
+    'linux': ['GL', 'X11'],
+    # 'darwin': [],
+}
+
+extra_compile_args = {
+    'windows': [],
+    'linux': ['-fpermissive'],
+    # 'darwin': ['-Wno-deprecated-declarations'],
+}
+
+extra_linker_args = {
+    'windows': [],
+    'linux': [],
+    # 'darwin': ['-framework', 'OpenGL', '-Wno-deprecated'],
+}
+
+target = platform.system().lower()
+
+if target not in PLATFORMS:
+    target = 'linux'
+
+long_description = open('README.md').read()
+
+glwindow = Extension(
+    name='glwindow',
+    sources=[
+        'glwindow/glwindow.cpp',
+        'glwindow/core_%s.cpp' % target,
+        'res/glwindow.rc',
+    ],
+    extra_compile_args=extra_compile_args[target],
+    extra_linker_args=extra_linker_args[target],
+    libraries=libraries[target],
+)
+
+setup(
+    name='glwindow',
+    version='3.0.0',
+    description='glwindow',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/cprogrammer1994/glwindow',
+    author='Szabolcs Dombi',
+    author_email='cprogrammer1994@gmail.com',
+    license='MIT',
+    ext_modules=[glwindow],
+    install_requires=['moderngl'],
+)
