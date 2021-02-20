06ebb46b7bd9d421627435c75cfa220add9accfb
Szabolcs Dombi <cprogrammer1994@gmail.com>
2021-02-20 10:02:03 +0200

    4.1.2

--- setup.py
+++ setup.py
@@ -6,21 +6,21 @@ long_description = open('README.md').read()
 if sys.platform == 'win32':
     glwindow = Extension(
         name='glwindow',
-        sources=['./glwindow.cpp'],
+        sources=['src/glwindow.cpp'],
         libraries=['user32'],
     )
 
 if sys.platform == 'linux':
     glwindow = Extension(
         name='glwindow',
-        sources=['./glwindow_linux.cpp'],
+        sources=['src/glwindow_linux.cpp'],
         libraries=['X11'],
         extra_compile_args=['-fpermissive'],
     )
 
 setup(
     name='glwindow',
-    version='4.1.1',
+    version='4.1.2',
     description='glwindow',
     long_description=long_description,
     long_description_content_type='text/markdown',
